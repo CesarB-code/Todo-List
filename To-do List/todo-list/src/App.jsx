48abedcc9f290b308818f08408b5c80d0e3158dd
Cesar Barrera <70235669+CesarB-code@users.noreply.github.com>
2023-10-09 13:24:58 -0700

    new

---
+++ To-do List/todo-list/src/App.jsx
@@ -0,0 +1,20 @@
+
+
+import { CssBaseline } from '@mui/material'
+import TodoList from './TodoList'
+import NavBar from './NavBar'
+
+
+function App() {
+
+  return (
+    <>
+      <CssBaseline />
+      <NavBar />
+
+      <TodoList />
+    </>
+  )
+}
+
+export default App
